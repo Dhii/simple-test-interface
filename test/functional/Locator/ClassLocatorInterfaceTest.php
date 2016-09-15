7b053025f2b7dc9ea412476549788c718e86776f
Anton Ukhanev <xedin.unknown@gmail.com>
2016-09-15 15:10:28 +0200

    Added test

---
+++ test/functional/Locator/ClassLocatorInterfaceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dhii\SimpleTest\FuncTest\Locator;
+
+/**
+ * Tests {@see \Dhii\SimpleTest\Locator\LocatorInterface}.
+ *
+ * @since [*next-version*]
+ */
+class LocatorInterfaceTest extends \Xpmock\TestCase
+{
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \Dhii\SimpleTest\Locator\LocatorInterface The new instance of the subject.
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock('Dhii\\SimpleTest\\Locator\\LocatorInterface')
+                ->locate()
+                ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests that a class implementing this interface can be instantiated.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf('Dhii\\SimpleTest\\Locator\\LocatorInterface', $subject, 'Subject is not a valid locator');
+    }
+}
