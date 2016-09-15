315378b78b1847f1ee87369607c0d94b9e4a8cb7
Anton Ukhanev <xedin.unknown@gmail.com>
2016-09-15 14:55:51 +0200

    Added test.

---
+++ test/functional/Assertion/AccountableInterfaceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dhii\SimpleTest\FuncTest\Assertion;
+
+/**
+ * Tests {@see \Dhii\SimpleTest\Assertion\AccountableInterface}.
+ * 
+ * @since [*next-version*]
+ */
+class AccountableInterfaceTest extends \Xpmock\TestCase
+{
+    /**
+     * Creates a new instance of the test subject.
+     * 
+     * @since [*next-version*]
+     * 
+     * @return \Dhii\SimpleTest\Assertion\AccountableInterface The new instance of the subject.
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock('Dhii\\SimpleTest\\Assertion\\AccountableInterface')
+                ->getAssertionCount()
+                ->new();
+        
+        return $mock;
+    }
+    
+    /**
+     * Tests that a class implementing this interface can be instantiated.
+     * 
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+        
+        $this->assertInstanceOf('Dhii\\SimpleTest\\Assertion\\AccountableInterface', $subject, 'Subject is not a valid assertion accountable');
+    }
+}
