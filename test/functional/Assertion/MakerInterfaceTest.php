c9c65cce312570290525ad550325c461d37185cc
Anton Ukhanev <xedin.unknown@gmail.com>
2016-09-15 15:00:49 +0200

    Added test

---
+++ test/functional/Assertion/MakerInterfaceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Dhii\SimpleTest\FuncTest\Assertion;
+
+/**
+ * Tests {@see \Dhii\SimpleTest\Assertion\MakerInterface}.
+ *
+ * @since [*next-version*]
+ */
+class MakerInterfaceTest extends \Xpmock\TestCase
+{
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return \Dhii\SimpleTest\Assertion\MakerInterface The new instance of the subject.
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock('Dhii\\SimpleTest\\Assertion\\MakerInterface')
+                ->make()
+                ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests that a class implementing this interface can be instantiated.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf('Dhii\\SimpleTest\\Assertion\\MakerInterface', $subject, 'Subject is not a valid failed exception');
+    }
+}
